8ab90a5342f176ca3adf994538d78fc498335a7b
Carlos Jaime <jaime_andrek@hotmail.com>
2022-11-25 19:49:48 -0300

    Enum graphql

--- data/db.js
+++ data/db.js
@@ -7,21 +7,24 @@ const usuarios = [{
     id: 1,
     nome: "joão",
     email: "joão@gmail.com",
-    perfil_id: 1
+    perfil_id: 1,
+    status: "ATIVO"
 },
 {
     id: 2,
     nome: "joão2",
     email: "joão2@gmail.com",
     idade: 30,
-    perfil_id: 2
+    perfil_id: 2,
+    status: "INATIVO"
 },
 {
     id: 3,
     nome: "joão3",
     email: "joão3@gmail.com",
     idade: 31,
-    perfil_id: 1
+    perfil_id: 1,
+    status: "BLOQUEADO"
 }]
 
 module.exports = {
