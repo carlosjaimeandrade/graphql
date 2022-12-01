632caba9680b1ba14820fd3e3dc520113af7697b
Carlos Jaime <jaime_andrek@hotmail.com>
2022-12-01 18:44:53 -0300

    criando função para alterar id automaticamente

--- mutations/data/db.js
+++ mutations/data/db.js
@@ -1,24 +1,30 @@
+let id= 1
+function proximoNumero(){
+    return id++
+}
+
+
 const perfis = [
     { id: 1, nome: 'comum' },
     { id: 2, nome: 'administrador' }
 ]
 
 const usuarios = [{
-    id: 1,
+    id: proximoNumero(),
     nome: 'João Silva',
     email: 'jsilva@zemail.com',
     idade: 29,
     perfil_id: 1,
     status: 'ATIVO'
 }, {
-    id: 2,
+    id: proximoNumero(),
     nome: 'Rafael Junior',
     email: 'rafajun@wemail.com',
     idade: 31,
     perfil_id: 2,
     status: 'INATIVO'
 }, {
-    id: 3,
+    id: proximoNumero(),
     nome: 'Daniela Smith',
     email: 'danismi@umail.com',
     idade: 24,
